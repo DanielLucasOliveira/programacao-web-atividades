435020707336401bbcbf7955760129e7ba9e5928
Daniel Lucas <daniel.lucas1507@gmail.com>
2023-08-16 10:38:56 -0300

    faltava a verificação de tamanhos das matrizes

--- atividade2/exercicio3.js
+++ atividade2/exercicio3.js
@@ -1,6 +1,9 @@
 const matrizUm = [1, 2, 3, 4, 5, 6]
 const matrizDois = [5, 5, 5, 5 ,5, 5]
-
+if(matrizUm.length != matrizDois.length){
+    console.log("Matrizes de tamanho diferente, não é possível calcular");
+    return false;
+}
 for(i = 0; i < matrizUm.length; i++){
     console.log(`Resultado da multiplicação da Matriz A e B = ${matrizUm[i] * matrizDois[i]}`);
 }
