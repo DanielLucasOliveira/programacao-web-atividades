155398e5b0944c516e3ec4c0b700afdda7fba3d2
Daniel Lucas <daniel.lucas1507@gmail.com>
2023-08-16 14:58:45 -0300

    arrumando o exercício 2

--- atividade2/exercicio2.js
+++ atividade2/exercicio2.js
@@ -1,8 +1,11 @@
-const matriz = [1, 2, 3, 4, 5, 6]
-for(let i = 0; i < matriz.length; i += 2){
+const matriz = [1, 2, 3, 4, 5, 6];
+
+console.log("Matriz Original:");
+for (let i = 0; i < matriz.length; i += 2) {
     console.log(matriz[i], matriz[i + 1]);
 }
-console.log("Transposta: ");
-for(let i = 0; i < matriz.length; i += 3){
-    console.log(matriz[i], matriz[i + 1], matriz[i + 2]);
+
+console.log("Matriz Transposta:");
+for (let i = 0; i < 2; i++) {
+    console.log(matriz[i], matriz[i + 2], matriz[i + 4]);
 }
