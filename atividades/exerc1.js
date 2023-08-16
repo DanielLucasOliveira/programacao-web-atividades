d353aadf647d6b06c679fd0544eaf903adddc27c
Daniel Lucas <daniel.lucas1507@gmail.com>
2023-08-16 09:49:09 -0300

    ajustando exercício

--- atividades/exerc1.js
+++ atividades/exerc1.js
@@ -1,7 +1,8 @@
-let num = 25
-
-if(num % 2 == 0){
-    console.log("Numero Par");
-}else {
-    console.log('Numero Impar');
-}
+function numeroPrimo(num){
+    for(var i = 3; i<num; i+=2){
+        if( num % i === 0 || num % ( i + 2) === 0){
+            return console.log("Não é primo");
+        }
+        console.log(num, "é primo");
+    }
+}console.log(numeroPrimo(13));
