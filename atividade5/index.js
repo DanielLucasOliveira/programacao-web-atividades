9eeb6bad3b1cc7eab087c66eb151e06fdfcbd423
DANIEL LUCAS DE OLIVEIRA <daniel.loliveira@a.ucb.br>
2023-09-27 10:37:59 -0300

    atualizando atividade

--- atividade5/index.js
+++ atividade5/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const app = express();
-const bodyParser = require("body-parser");
+
 
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", function(req, res){
     
