15279c143cc4646b79537caad25afddf224fcb5b
DANIEL LUCAS DE OLIVEIRA <daniel.loliveira@a.ucb.br>
2023-09-27 10:27:08 -0300

    ajustes atividade 5

--- atividade5/index.js
+++ atividade5/index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const app = express();
-const body_parser = require('body-parser')
+const bodyParser = require("body-parser");
+
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views')
-app.use(body_parser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get("/:nome", function(req, res){
-    let nome = req.params.nome;
-    res.render("index.html", {nome});
+app.get("/", function(req, res){
+    
+    res.render("index.html");
 });
 
 app.post("/formulario", function(req, res){
     let nome = req.body.nome
     let endereco = req.body.endereco
     let data = req.body.dataAgendamento
+    data = new Date(data);
+    data = data.toLocaleDateString();
     let tel = req.body.tel
     res.render("formReceive.html", {nome, endereco, data, tel});
 })
